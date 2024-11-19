a083b7a2c20b2fc57e76c141fb4a41ff06008ade
Vincentius Filbert <vincentius.filbert@ui.ac.id>
2024-11-19 18:31:11 +0700

    validate access

--- internal/handlers/access/createAccess.go
+++ internal/handlers/access/createAccess.go
@@ -7,12 +7,37 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 func CreateAccess(c *fiber.Ctx) error {
 	db := database.DB
 
 	access := new(AccessCreateRequest)
+
+	var witch model.Witch
+	if err := db.First(&witch, "id = ?", access.WitchID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"message": "Witch not found",
+			})
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to query witch",
+		})
+	}
+
+	var magicBook model.MagicBook
+	if err := db.First(&magicBook, "id = ?", access.MagicBookID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"message": "Magic book not found",
+			})
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to query magic book",
+		})
+	}
 	if err := c.BodyParser(access); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": "Malformed request",
